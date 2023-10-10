6e2f4754e8dcb9ce8c395808344ecf2540fb261d
iaio998 <depalatisiacopo@gmail.com>
2023-10-10 14:18:07 +0200

    Added HTML main structure

--- javascript/script.js
+++ javascript/script.js
@@ -2,9 +2,9 @@
 // Scrivere un programma che chieda all’utente:
 // - Il numero di chilometri da percorrere
 // - Età del passeggero
-let kilometers = parseInt(prompt("Inserire numero km da percorrere."));
-let age = parseInt(prompt("Inserire l'eta' del passeggero."));
-console.log(kilometers, age);
+// let kilometers = parseInt(prompt("Inserire numero km da percorrere."));
+// let age = parseInt(prompt("Inserire l'eta' del passeggero."));
+// console.log(kilometers, age);
 
 // SICUREZZA DI RIEMPIMENTO
 if (isNaN(kilometers) || isNaN(age)) {
@@ -35,6 +35,7 @@ else if (age >= 65) {
 
 // MILESTONE 1:
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+
 // MILESTONE 2:
 // Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 // Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
